605e9e0bd0874ac92ed1be09e3ab6b8656e3c38c
waldo.wangy <waldo.wangy@alibaba-inc.com>
2017-03-05 22:21:59 +0800

    daily ci

---
+++ j2ee/src/main/java/org/waldo/demo/javax/validation/Car.java
@@ -0,0 +1,13 @@
+/**
+ * @author waldo.wy
+ * @date 2017/3/3
+ */
+package org.waldo.demo.javax.validation;
+
+/**
+ * 类Car的实现描述：TODO 类实现描述
+ *
+ * @author waldo.wangy 2017/3/3 15:03
+ */
+public class Car {
+}
---
+++ j2ee/src/test/java/org/waldo/demo/javax/validation/CarTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author waldo.wy
+ * @date 2017/3/3
+ */
+public class CarTest {
+
+}
