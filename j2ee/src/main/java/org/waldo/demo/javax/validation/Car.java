f3b71e41c5feac5497c11df132955fb7b4a0243d
waldo.wangy <waldo.wangy@alibaba-inc.com>
2017-03-06 10:18:20 +0800

    daily ci

--- j2ee/src/main/java/org/waldo/demo/javax/validation/Car.java
+++ j2ee/src/main/java/org/waldo/demo/javax/validation/Car.java
@@ -4,10 +4,57 @@
  */
 package org.waldo.demo.javax.validation;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
- * 类Car的实现描述：TODO 类实现描述
+ * Validation验证实体类
  *
  * @author waldo.wangy 2017/3/3 15:03
  */
 public class Car {
+
+    @NotNull
+    private String manufacturer;
+
+    @NotNull
+    @Size(min = 2, max = 14)
+    private String licensePlate;
+
+    @Min(2)
+    private int seatCount;
+
+    public Car() {
+    }
+
+    public Car(String manufacturer, String licensePlate, int seatCount) {
+        this.manufacturer = manufacturer;
+        this.licensePlate = licensePlate;
+        this.seatCount = seatCount;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public int getSeatCount() {
+        return seatCount;
+    }
+
+    public void setSeatCount(int seatCount) {
+        this.seatCount = seatCount;
+    }
 }
