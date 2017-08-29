21f220f68bad7a740d3fe8ce40e2c27b6e4a3928
waldo.wangy <waldo.wangy@alibaba-inc.com>
2017-08-29 14:57:31 +0800

    daily ci

---
+++ foundation/src/main/java/org/waldo/demo/foundation/string/OriginalFormatter.java
@@ -0,0 +1,25 @@
+package org.waldo.demo.foundation.string;
+
+import java.text.MessageFormat;
+import java.util.Date;
+
+/**
+ * @author waldo.wangy 2017/8/21
+ */
+public class OriginalFormatter {
+
+    public static void main(String[] args) {
+        int planet = 7;
+        String event = "a disturbance in the Force";
+
+        String messageFormatResult = MessageFormat.format(
+                "At {1,time} on {1,date}, there was {2} on planet {0,number,integer}.",
+                planet, new Date(), event);
+
+        System.out.println(messageFormatResult);
+
+        String stringFormatResult = String.format("At %2$tH:%2$tM:%2$tS on %2$tY-%2$tm-%2$td, there was %3$s on planet %1$d.", planet, new Date(), event);
+        System.out.println(stringFormatResult);
+    }
+
+}
